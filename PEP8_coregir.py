1a530e14c2378a065a046400f3affd8cd624ee11
jcardonhid <jcardonhid@educacion.navarra.es>
2024-10-17 20:01:15 +0200

    Update PEP8_coregir.py
    Modificación línea 5: separación del signo igual.

--- PEP8_coregir.py
+++ PEP8_coregir.py
@@ -2,7 +2,7 @@
 import os
 import sys
 def MiFuncionSuma(A, B, C, imprime = True):
-    resultado=A+B+C
+    resultado = A+B+C
     if imprime != False:
         print(resultado)
     return resultado
